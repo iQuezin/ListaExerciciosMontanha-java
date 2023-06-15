f9d3d5ee6124d519a6874dba06b8987616599761
Kayque dos Santos <129982745+iQuezin@users.noreply.github.com>
2023-06-15 12:26:24 -0300

    Delete 01EstruturasSequenciais directory

--- 01EstruturasSequenciais/src/ExercicioUm.java
+++
@@ -1,7 +0,0 @@
-//1. Faça um Programa que mostre a mensagem "Alo mundo" na tela.
-
-public class ExercicioUm {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Alo Mundo");
-    }
-}
